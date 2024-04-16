e1149b29715f1a7662b0eb3cdb34a47442000562
Aytac Allahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-04-16 20:19:19 +0400

    add all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
 
 function App() {
+
+  // let count = 0;
+  const [count, setCount] = React.useState(0);
+
+  const onclickPlus = () => {
+    setCount(count + 1)
+  }
+
+  const onclickMinus = () => {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={onclickMinus} className="minus">- Минус</button>
+        <button onClick={onclickPlus} className="plus">Плюс +</button>
+      </div>
     </div>
   );
 }
